0bbce6393bf4e2cf269452e9fb39bdb3c012676f
mohammad sihan <mohammadsihan1@gmail.com>
2023-02-07 13:23:07 +0600

    first commit

---
+++ problem.js
@@ -0,0 +1,108 @@
+// Problem 1: Let’s play a mind game:::
+
+/* description:
+This function will help someone to calculate a number with various types of numbers with many condition. Here 'mindGame' is the function name, 'let' is the variable and 'positive_number' is the function parameter.
+*/
+
+function mindGame(positive_number){
+    
+    return(num);
+
+}
+    let num = 10;
+    num = num*3;
+    num = num+10;
+    num = num/2;
+    num = num-5;
+    
+ /*--------------------------------------X------------------------------------*/
+
+
+// Problem 2: Finding even or odd:::
+
+/*Description:
+This function will help someone to declare a length of a string type variable.  
+Here 'evenOdd' is the function name, 'let' is the variable, 'String' is the function parameter.
+*/
+
+function evenOdd(String){
+
+    if(text.length == 18){
+       return('even');
+    }
+    else{
+        return('odd');
+    }
+
+}
+
+    let text ='Jhankar Mahbub sir';
+
+  /*-------------------------------------X-----------------------------------*/
+
+
+// Problem 3: Is Less or Greater than seven:::
+
+/*Description:
+This function will help someone to compare any negative or positive number with seven. Here 'isLGSeven' is the function name, 'let' is the variable, 'a_number' is the function parameter.
+*/
+
+function isLGSeven(a_number){
+    if(newestNumber < 7){
+        return(newestNumber);
+    }
+    else{
+        return(newNumber*2);
+    }
+}
+
+    let newNumber = 10;
+    let newerNumber = 7 - newNumber;
+    let newestNumber = newerNumber;
+
+ /*-----------------------------------X---------------------------------------*/
+
+
+// Problem 4: Finding Bad data:::
+
+/*Description:
+This function will help someone to find the negative numbers(BadData actually) from an array. Here 'findingBadData' is the function name, 'let' is the variable.
+*/
+
+function findingBadData(){
+    if(whatArrayIncludes == true){
+        return(3);
+    }
+    else{
+        return(0);
+    }
+}
+
+    let anArray = [12, 4, 8, -3, -6, -13];
+    let whatArrayIncludes = anArray.includes(-3, -6, -13); 
+    findingBadData()
+ /*-----------------------------------X---------------------------------------*/
+
+
+// Problem 5: Convert your gems into diamond:::
+
+/*Description:
+This function will help someone to handle multiple parameters and a condition. Here 'gemsToDiamond' is the function, 'firstFrnd, secondFrnd, thirdFrnd' are the function parameters and 'let' is the variable.
+*/
+
+function gemsToDiamond(firstFrnd, secondFrnd, thirdFrnd){
+    if(friendsTotalDiamonds > 200){
+        console.log(friendsTotalDiamonds-2000);
+    }
+}
+    let firstFriendGems = 25;
+    let secondFriendGems = 24;
+    let thirdFriendGems = 29;
+
+    let firstFriendDiamonds = 25*21;
+    let secondFriendDiamonds = 24*32;
+    let thirdFriendDiamonds = 29*43;
+
+    let friendsTotalDiamonds = firstFriendDiamonds + secondFriendDiamonds + thirdFriendDiamonds;
+
+ /*----------------------------------X----------------------------------------*/
